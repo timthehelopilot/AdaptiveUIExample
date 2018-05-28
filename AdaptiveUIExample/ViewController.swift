0a8e23fb0eb7d6ff518f07248626ce1230936c6f
Timothy Barrett <tim.barrett.jr@gmail.com>
2018-05-28 15:59:46 -0700

    Initial Commit

---
+++ AdaptiveUIExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveUIExample
+//
+//  Created by Tim Barrett on 5/28/18.
+//  Copyright © 2018 Tim Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
